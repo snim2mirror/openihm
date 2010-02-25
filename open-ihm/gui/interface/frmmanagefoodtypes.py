0ad796a533911a3d6772ab39cdb4f985558724a6
Tiwonge Manda <tiomanda@gmail.com>
2010-02-25 16:18:53 +0200

    Adding Data management interfaces

--- open-ihm/gui/interface/frmmanagefoodtypes.py
+++ open-ihm/gui/interface/frmmanagefoodtypes.py
@@ -1,7 +1,15 @@
-from PyQt4 import QtGui, QtCore
-from gui.designs.ui_managefoodtypes import Ui_foodTypes
+#-----------------------------------------------------------------------------------------------	
+#	Filename: frmmanagefoodtypes.py
+#
+#	Class to create the form for adding, editing, or deleting Food Types - FrmManageFoodTypes.
+#------------------------------------------------------------------------------------------------
 
-class FrmManageFoodTypes(Ui_foodTypes):		
-    def setupUi(self,Form,Mdi):
-        Ui_foodTypes.setupUi(self,Form)
-        QtCore.QObject.connect(self.btnManageFoodCancel, QtCore.SIGNAL("clicked()"), Mdi.closeActiveSubWindow)
+from PyQt4 import QtGui, QtCore
+from gui.designs.ui_managefoodtypes import Ui_FoodTypes
+
+class FrmManageFoodTypes(Ui_FoodTypes):		
+	def setupUi(self,Form,Mdi):
+		Ui_FoodTypes.setupUi(self,Form)
+		
+		QtCore.QObject.connect(self.btnManageFoodClose, QtCore.SIGNAL("clicked()"), Mdi.closeActiveSubWindow)
+
