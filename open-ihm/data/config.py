611da146d4200e1f5e571e9bea5a5dfaf95d60a3
Brown  Msiska <bmsiska@gmail.com>
2010-04-19 22:50:03 +0200

    Adding connector configuration file.

---
+++ open-ihm/data/config.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+class Config(object):
+    """ Configures the mysql database connector which is then called as below:
+    
+        mysql.connector.Connect(**Config.dbinfo())
+    """
+    
+    HOST = 'localhost'
+    DATABASE = 'openihmdb'
+    USER = 'openihm'
+    PASSWORD = 'ihm2010'
+    PORT = 3306
+    
+    CHARSET = 'utf8'
+    UNICODE = True
+    WARNINGS = True
+    
+    @classmethod
+    def dbinfo(cls):
+        return {
+            'host'          : cls.HOST,
+            'database'      : cls.DATABASE,
+            'user'          : cls.USER,
+            'password'      : cls.PASSWORD,
+            'charset'       : cls.CHARSET,
+            'use_unicode'   : cls.UNICODE,
+            'get_warnings'  : cls.WARNINGS,
+            }
+    
