4757a728f7ba6af7b5840ade01fca13bbc47550c
Tiwonge Manda <tiomanda@gmail.com>
2010-06-01 10:01:46 +0200

    updating code for manageassets

--- open-ihm/gui/interface/frmmanageassets.py
+++ open-ihm/gui/interface/frmmanageassets.py
@@ -24,16 +24,16 @@ class FrmManageAssetDetails(QDialog, Ui_ManageAssetDetails):
         	QDialog.__init__(self)
        		self.setupUi(self)
         	self.parent = parent
-
-        	self.model = QStandardItemModel()
-        	self.listView.setModel(self.model)
         			
 		# get asset types
         	self.getAssetCategories()
 
 		# connect relevant signals and slots
 		#self.connect(self.btnAssetsClose, SIGNAL("clicked()"), self.parent.mdi.closeActiveSubWindow)
-		self.connect(self.listView.selectionModel(), SIGNAL("clicked(QModelIndex)"), self.manageCategories(QModelIndex))
+		#self.connect(self.listView.selectionModel(), SIGNAL("currentChanged(QModelIndex,QModelIndex)"), self.manageCategories)
+		self.connect(self.btnCatSave, SIGNAL("clicked()"), self.saveCategoryType)
+		self.connect(self.btnCatDelete, SIGNAL("clicked()"), self.deleteCategoryType)
+		self.connect(self.listView, SIGNAL("clicked(QModelIndex)"), self.getCategories)
 
 	def getAssetCategories(self):
                	# select query to retrieve Asset Categories
@@ -42,20 +42,80 @@ class FrmManageAssetDetails(QDialog, Ui_ManageAssetDetails):
                 p = GenericDBOP(query)
                 recordset = p.runSelectQuery()
 				
+		model = QStandardItemModel()
 		num = 0
 
        		for row in recordset:
 			qtAssetType = QStandardItem( "%s" % row[0])
             		qtAssetType.setTextAlignment( Qt.AlignLeft )
-            		self.model.setItem( num, 0, qtAssetType )
+            		model.setItem( num, 0, qtAssetType )
             		num = num + 1
                         		
-        	
-		#self.listView.show()
+        	self.listView.setModel(model)
+		self.listView.show()
 
-        def manageCategories(self,index1):
-                tem = QStandardItem
-                tem = str(self.model.index(index1).text())
-                print item
-               # self.txtAssetCategories.setText(int( self.model.itemFromIndex(0,0).text() ))
+        def getCategories(self,index):
+                #get selected item and populate categories textbox
+                selectedItem = self.listView.model().item(index.row(),0).text()
+                self.txtAssetCategories.setText(selectedItem)
                 
+        def saveCategoryType(self):
+        	''' Saves newly created data to database '''
+
+        	# get the data entered by user
+        	categorytype = self.txtAssetCategories.text()		
+        	
+		# check if record exists
+		query = '''SELECT assettype FROM assettypes WHERE assettype='%s' ''' % (categorytype)    
+		
+		p = GenericDBOP(query)
+                recordset = p.runSelectQuery()
+
+		numrows = 0		
+		for row in recordset:
+			numrows = numrows + 1
+				      	
+		if numrows == 0:
+			
+			query = '''INSERT INTO assettypes(assettype) 
+                     		VALUES('%s')''' % (categorytype)
+		else:
+			query = '''UPDATE assettypes SET assettype='%s'	WHERE assettype='%s' ''' % (categorytype, categorytype)
+    
+        	# execute query and commit changes
+        	temp = GenericDBOP(query)
+                recordset = temp.runUpdateQuery()
+		#refresh categories list
+		#self.getCategories()                
+                
+	def deleteCategoryType(self):
+		''' Deletes record from database '''
+
+        	# get the data entered by user
+        	categorytype = self.txtAssetCategories.text()		
+        	
+		# check if record exists
+		query = '''SELECT assettype FROM assettypes WHERE assettype='%s' ''' % (categorytype)    
+		
+		p = GenericDBOP(query)
+                recordset = p.runSelectQuery()
+		numrows = 0		
+		for row in recordset:
+			numrows = numrows + 1
+
+		if numrows <> 0:
+			
+			query = '''DELETE FROM assettypes WHERE assettype='%s' ''' % (categorytype)
+
+			# execute query and commit changes
+        		temp = GenericDBOP(query)
+                        recordset = temp.runUpdateQuery()
+
+			
+        		#self.cmbKCalories.clear()
+			
+			#refresh categories list
+			self.getCategories()			
+			
+		else:
+			QMessageBox.information(self, 'Delete Food Type', "Record not found")
