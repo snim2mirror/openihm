f9a2ef02b22b84a88aa25232083d78c0388d1ffd
Brown Msiska <bmsiska@gmail.com>
2010-10-20 08:16:41 +0200

    Modified livestock income data entry code so that unit of measure is automatically picked up.

--- open-ihm/gui/interface/frmhousehold_addincome_livestock.py
+++ open-ihm/gui/interface/frmhousehold_addincome_livestock.py
@@ -12,102 +12,126 @@ import data.mysql.connector
 from gui.designs.ui_household_income_livestock import Ui_AddHouseholdIncomeLivestock
 
 class FrmHouseholdLivestockIncome(QDialog, Ui_AddHouseholdIncomeLivestock):	
-    ''' Form to add or edit a Household Livestock Income  '''	
-    def __init__(self, parent,  hhid, hhname, incomeid = 0 ):
-		''' Set up the dialog box interface '''
-		QDialog.__init__(self)
-		self.setupUi(self)
-		self.parent 	= parent
-		self.hhid 		= hhid
-		self.pid           = parent.parent.projectid
-		self.incomeid 	= incomeid
-		
-		self.config = Config.dbinfo().copy()
-		
-		self.getLivestockTypes()
-		
-		if ( incomeid != 0 ):
-			self.displayIncomeDetails()
-			self.setWindowTitle( "Edit Income Item" )
-		  
-		# display household name
-		self.lblHouseholdName.setText(hhname)
-		
-		# connect relevant signals and slots
-		self.connect(self.cmdCancel, SIGNAL("clicked()"), self.close)
-		self.connect(self.cmdSave, SIGNAL("clicked()"), self.saveIncome)
+     ''' Form to add or edit a Household Livestock Income  '''	
+     def __init__(self, parent,  hhid, hhname, incomeid = 0 ):
+         ''' Set up the dialog box interface '''
+         QDialog.__init__(self)
+         self.setupUi(self)
+         self.parent 	= parent
+         self.hhid 		= hhid
+         self.pid = parent.parent.projectid
+         self.incomeid 	= incomeid
+         
+         self.config = Config.dbinfo().copy()
+         
+         self.getLivestockTypes()
+         
+         if ( incomeid != 0 ):
+             self.displayIncomeDetails()
+             self.setWindowTitle( "Edit Income Item" )
+             
+         # display household name
+         self.lblHouseholdName.setText(hhname)
+         
+         # lock editing of income source and unit of measure
+         self.cboIncomeType.setEditable( False )
+         self.txtUnitOfMeasure.setReadOnly( True )
+         
+         # connect relevant signals and slots
+         self.connect(self.cboIncomeType, SIGNAL("currentIndexChanged(int)"), self.displayUnitOfMeasure)
+         self.connect(self.cmdCancel, SIGNAL("clicked()"), self.close)
+         self.connect(self.cmdSave, SIGNAL("clicked()"), self.saveIncome)
+         
+     def displayUnitOfMeasure(self):
+         ''' displays the unit of measure of the selected income source '''
+         unitofmeasure = self.cboIncomeType.itemData( self.cboIncomeType.currentIndex() ).toString()
+         self.txtUnitOfMeasure.setText( unitofmeasure )
         
-    def getLivestockTypes(self):
-		''' Retrieve Livestock Types and display them in a combobox '''
-		# select query to Livestock Types
-		query = '''SELECT incomesource FROM setup_livestock'''
-		
-		db = data.mysql.connector.Connect(**self.config)             
-		cursor = db.cursor()
-		
-		cursor.execute(query)
-		
-		for row in cursor.fetchall():
-		    incometype = row[0]
-		    self.cboIncomeType.addItem(incometype)
-		 
-		cursor.close()   
-		db.close()
+     def getLivestockTypes(self):
+         ''' Retrieve Livestock Types and display them in a combobox '''
+         # select query to Livestock Types
+         query = '''SELECT incomesource, unitofmeasure FROM setup_livestock'''
+
+         db = data.mysql.connector.Connect(**self.config)             
+         cursor = db.cursor()
+
+         cursor.execute(query)
+         
+         rows = cursor.fetchall()
+         
+         for row in rows:
+             incometype = row[0]
+             measuringunit = row[1]
+             self.cboIncomeType.addItem(incometype, QVariant(measuringunit))
+
+         unitofmeasure = self.cboIncomeType.itemData( self.cboIncomeType.currentIndex() ).toString()
+         self.txtUnitOfMeasure.setText( unitofmeasure )
+
+         cursor.close()   
+         db.close()
         
-    def displayIncomeDetails(self):
-		''' Retrieve and display Household Income details '''
-		query = '''SELECT * FROM livestockincome WHERE hhid=%s AND pid=%s AND id=%s ''' % ( self.hhid, self.pid, self.incomeid )
-		
-		db = data.mysql.connector.Connect(**self.config)             
-		cursor = db.cursor()
-		
-		cursor.execute(query)
-		
-		for row in cursor.fetchall():
-		    incometype = row[2]
-		    self.cboIncomeType.setCurrentIndex( self.cboIncomeType.findText( incometype ) )
-		    unitofmeasure = row[3]
-		    self.txtUnitOfMeasure.setText( unitofmeasure )
-		    unitsconsumed = row[4]
-		    self.txtUnitsConsumed.setText( str(unitsconsumed) )
-		    unitssold = row[5]
-		    self.txtUnitsSold.setText( str(unitssold) )
-		    unitprice = row[6]
-		    self.txtUnitPrice.setText( str(unitprice) )
-		 
-		cursor.close()   
-		db.close()
-        
-    def saveIncome(self):
-		''' Saves livestock income to database '''    	
-		
-		# get the data entered by user
-		incometype   	= self.cboIncomeType.currentText()
-		unitofmeasure	= self.txtUnitOfMeasure.text()
-		unitsconsumed	= self.txtUnitsConsumed.text()
-		unitssold		= self.txtUnitsSold.text()
-		unitprice		= self.txtUnitPrice.text()
-			
-		db = data.mysql.connector.Connect(**self.config)
-		
-		# create UPDATE query
-		if (self.incomeid == 0):
-			query = '''INSERT INTO livestockincome (hhid, incomesource, unitofmeasure, unitsconsumed, unitssold, unitprice, pid )
-			    VALUES(%s,'%s','%s',%s,%s,%s,%s) ''' % ( self.hhid, incometype, unitofmeasure, unitsconsumed, unitssold, unitprice,  self.pid )
-		else:
-			query = ''' UPDATE livestockincome SET incomesource='%s', unitofmeasure='%s', unitsconsumed=%s, unitssold=%s,
-						unitprice=%s WHERE hhid=%s AND pid=%s 
-						AND id=%s ''' % ( incometype, unitofmeasure, unitsconsumed, unitssold, unitprice, self.hhid, self.pid,  self.incomeid)
-		
-		# execute query and commit changes
-		cursor =  db.cursor()
-		cursor.execute(query)
-		db.commit()
-		
-		# close database connection
-		cursor.close()
-		db.close()
-		
-		# close new project window
-		self.parent.retrieveHouseholdLivestockIncome()
-		self.close()
+     def displayIncomeDetails(self):
+         ''' Retrieve and display Household Income details '''
+         query = '''SELECT incomesource, unitofmeasure, unitsproduced, unitssold, unitprice, otheruses, unitsconsumed  
+             FROM livestockincome WHERE hhid=%s AND pid=%s AND id=%s ''' % ( self.hhid, self.pid, self.incomeid )
+         
+         db = data.mysql.connector.Connect(**self.config)             
+         cursor = db.cursor()
+
+         cursor.execute(query)
+
+         for row in cursor.fetchall():
+             croptype = row[0]
+             self.cboIncomeType.setCurrentIndex( self.cboIncomeType.findText( croptype ) )
+             unitofmeasure = row[1]
+             self.txtUnitOfMeasure.setText( unitofmeasure )
+             unitsproduced = row[2]
+             self.txtUnitsProduced.setText( str(unitsproduced) )
+             unitssold = row[3]
+             self.txtUnitsSold.setText( str(unitssold) )
+             unitprice = row[4]
+             self.txtUnitPrice.setText( str(unitprice) )
+             otheruses = row[5]
+             self.txtUnitsOtherUses.setText( str(otheruses) )
+             unitsconsumed = row[6]
+             self.txtUnitsConsumed.setText( str(unitsconsumed) )
+
+         cursor.close()   
+         db.close()
+
+     def saveIncome(self):
+         ''' Saves livestock income to database '''    	
+
+         # get the data entered by user
+         incometype      	= self.cboIncomeType.currentText()
+         unitofmeasure	= self.txtUnitOfMeasure.text()
+         unitsproduced    = self.txtUnitsProduced.text() if self.txtUnitsProduced.text() != "" else "0"
+         unitsconsumed	= self.txtUnitsConsumed.text() if self.txtUnitsConsumed.text() != "" else "0"
+         unitssold		= self.txtUnitsSold.text() if self.txtUnitsSold.text() != "" else "0"
+         unitprice		= self.txtUnitPrice.text() if self.txtUnitPrice.text() != "" else "0"
+         otheruses       = self.txtUnitsOtherUses.text() if self.txtUnitsOtherUses.text() != "" else "0"
+
+         db = data.mysql.connector.Connect(**self.config)
+
+         # create UPDATE query
+         if (self.incomeid == 0):
+             query = '''INSERT INTO livestockincome (hhid, incomesource, unitofmeasure, unitsproduced, unitssold, unitprice, 
+                 otheruses, unitsconsumed, pid ) VALUES(%s,'%s','%s',%s,%s,%s, %s, %s, 
+                 %s) ''' % ( self.hhid, incometype, unitofmeasure, unitsproduced, unitssold, unitprice, otheruses, unitsconsumed, self.pid )
+         else:
+             query = ''' UPDATE livestockincome SET incomesource='%s', unitofmeasure='%s', unitsproduced=%s, unitssold=%s,
+                  unitprice=%s, otheruses=%s, unitsconsumed=%s WHERE hhid=%s AND pid=%s AND  
+                  id=%s ''' % ( incometype, unitofmeasure, unitsproduced, unitssold, unitprice, otheruses, unitsconsumed, self.hhid, self.pid,  self.incomeid)
+
+         # execute query and commit changes
+         cursor =  db.cursor()
+         cursor.execute(query)
+         db.commit()
+
+         # close database connection
+         cursor.close()
+         db.close()
+
+         # close new project window
+         self.parent.retrieveHouseholdLivestockIncome()
+         self.close()
