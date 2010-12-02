f87c53705d692212028c7907899d8093dcfc80b6
Brown Msiska <bmsiska@gmail.com>
2010-12-02 18:24:40 +0200

    [Issue 27] Updating database scripts with respect to changes to standard of living

--- open-ihm/gui/interface/frmproject_configure.py
+++ open-ihm/gui/interface/frmproject_configure.py
@@ -78,6 +78,165 @@ class FrmConfigureProject(QDialog, Ui_ProjectConfiguration):
         return selectedRows
         
     #--------------------------------------------------------------------------------------------------------------------------
+    #  Standard of Living
+    #-------------------------------------------------------------------------------------------------------------------------
+         
+    def getExpenseItems(self):
+         ''' Retrieve Expense Items and display them in a combobox '''
+         # select query to Crop Types
+         query = '''SELECT foodtype, measuringunit FROM setup_crops'''
+
+         db = data.mysql.connector.Connect(**self.config)             
+         cursor = db.cursor()
+
+         cursor.execute(query)
+
+         for row in cursor.fetchall():
+             croptype = row[0]
+             measuringunit = row[1]
+             self.cmbFoodItem.addItem(croptype, QVariant(measuringunit))
+
+         unitofmeasure = self.cmbFoodItem.itemData( self.cmbFoodItem.currentIndex() ).toString()
+         self.txtUnitOfMeasure.setText( unitofmeasure )
+
+         cursor.close()   
+         db.close()
+    
+    def displayStandardOfLiving(self):
+         ''' List available currencies '''
+         # select query to retrieve currencies
+         query = '''SELECT id, fooditem, unitofmeasure, percentage, priceperunit FROM diet '''
+         
+         # retrieve and display members
+         db = data.mysql.connector.Connect(**self.config)             
+         cursor = db.cursor()
+         
+         cursor.execute(query)
+         
+         model = QStandardItemModel(1,2)
+         
+         # set model headers
+         model.setHorizontalHeaderItem(0,QStandardItem('Id.'))
+         model.setHorizontalHeaderItem(1,QStandardItem('Food Item'))
+         model.setHorizontalHeaderItem(2,QStandardItem('Unit Of Measure'))
+         model.setHorizontalHeaderItem(3,QStandardItem('Percentage'))
+         model.setHorizontalHeaderItem(4,QStandardItem('Price per Unit'))
+         
+         # add  data rows
+         num = 0
+         
+         for row in cursor.fetchall():
+             qtID = QStandardItem("%i" % row[0])
+             qtID.setTextAlignment( Qt.AlignCenter )
+             qtFoodItem = QStandardItem( row[1] )	
+             qtUnit = QStandardItem(row[2] )
+             
+             qtPercentage = QStandardItem( "%.2f" %   row[3] )
+             qtPrice = QStandardItem( "%.2f" %   row[4] )
+             			
+             model.setItem( num, 0, qtID )
+             model.setItem( num, 1, qtFoodItem )
+             model.setItem( num, 2, qtUnit )
+             model.setItem( num, 3, qtPercentage )
+             model.setItem( num, 4, qtPrice )
+             num = num + 1
+             
+         cursor.close()   
+         db.close()
+         
+         self.tblDiets.setModel(model)
+         self.tblDiets.resizeColumnsToContents()
+         self.tblDiets.hideColumn(0)
+         self.tblDiets.show()
+         
+    def showStandardOfLivingItem(self, index):
+         ''' show details of a selected currency for editing '''
+         self.dietid = self.tblDiets.model().item(index.row(),0).text()
+         fooditem = self.tblDiets.model().item(index.row(),1).text()
+         unitofmeasure = self.tblDiets.model().item(index.row(),2).text()
+         percentage = self.tblDiets.model().item(index.row(),3).text()
+         priceperunit = self.tblDiets.model().item(index.row(),4).text()
+         
+         self.cmbFoodItem.setCurrentIndex(self.cmbFoodItem.findText( fooditem ))
+         self.txtUnitOfMeasure.setText(unitofmeasure)
+         self.txtPercentage.setText(percentage)
+         self.txtUnitPrice.setText(priceperunit)
+         
+    def saveStandardOfLivingItem(self):
+         ''' Save the currency details of a currency being added or edited '''
+         pid = self.parent.projectid
+         fooditem = self.cmbFoodItem.currentText()
+         unitofmeasure = self.txtUnitOfMeasure.text()
+         percentage = self.txtPercentage.text()
+         priceperunit = self.txtUnitPrice.text()
+         
+         db = data.mysql.connector.Connect(**self.config)
+         
+         # create INSERT or UPDATE query
+         if (self.dietid == 0):
+             query = '''INSERT INTO diet (pid, fooditem,unitofmeasure,percentage, priceperunit )
+                         VALUES(%s,'%s','%s',%s,%s) ''' % ( pid, fooditem,unitofmeasure,percentage, priceperunit  )
+         else:
+             query = ''' UPDATE diet SET fooditem='%s', unitofmeasure='%s', percentage=%s, priceperunit=%s
+                         WHERE id=%s AND pid=%s ''' % ( fooditem,unitofmeasure,percentage, priceperunit, self.dietid, pid)
+         
+         # execute query and commit changes
+         cursor =  db.cursor()
+         cursor.execute(query)
+         db.commit()
+         
+         # close database connection
+         cursor.close()
+         db.close()
+         
+         # clear text boxes and refresh list
+         self.txtPercentage.setText("")
+         self.txtUnitPrice.setText("")
+         self.dietid = 0
+         self.listDiets()
+         
+    def delStandardOfLivingItems(self):
+         ''' Delete a selected currencies '''
+         numSelected = self.countRowsSelected(self.tblDiets)
+         if  numSelected != 0:
+             # confirm deletion
+             if numSelected == 1:
+                 msg = "Are you sure you want to delete the selected diet item?"
+             else:
+                 msg = "Are you sure you want to delete the selected diet items?"
+             
+             ret = QMessageBox.question(self,"Confirm Deletion", msg, QMessageBox.Yes|QMessageBox.No)
+             # if deletion is rejected return without deleting
+             if ret == QMessageBox.No:
+                 return
+                 
+             # get the member id of the selected currencies
+             selectedRows = self.getSelectedRows(self.tblDiets)
+             selectedIds = []
+             for row in selectedRows:
+                 selectedIds.append( self.tblDiets.model().item(row,0).text() )
+             # delete selected currencies
+             
+             db = data.mysql.connector.Connect(**self.config)
+             cursor =  db.cursor()
+             
+             for dietid in selectedIds:
+                 query = '''DELETE FROM diet WHERE id='%s' ''' % (dietid)	
+                 cursor.execute(query)
+                 db.commit()
+    
+             # close database connection
+             cursor.close()
+             db.close()
+             
+             self.dietid = 0
+             self.listDiets()
+
+         else:
+             QMessageBox.information(self,"Delete Diet Items","Please select the rows containing diet items to be deleted.")
+       
+        
+    #--------------------------------------------------------------------------------------------------------------------------
     #  Diets
     #-------------------------------------------------------------------------------------------------------------------------
     
