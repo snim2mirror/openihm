0027d68d6a4ec4f4f789d5ed90de7aa295e7aa1d
brown <bmsiska@gmail.com>
2011-08-29 10:20:41 +0200

    Transferring Data from Access DB Phase Three: Importing Projects - Transfer Households - Commit #2.

--- open-ihm/control/transfermanager.py
+++ open-ihm/control/transfermanager.py
@@ -9,6 +9,15 @@ from model.database import Database
 from model.accessdb import AccessDB
 
 class TransferManager:
+     def logTransfer(self, pid, pid_access, projectname, startdate, currency ):
+         query = '''INSERT INTO transferlog(pid,pid_access,projectname,datecollected,currency) 
+                      VALUES(%s,%s,'%s','%s','%s')''' % (pid, pid_access, projectname, startdate, currency)
+                          
+         database = Database()
+         database.open()
+         database.execUpdateQuery( query )
+         database.close()
+         
      def getProjectsFromAccess(self, filename):
          query = 'SELECT ProjectID, ProjectName, DateOfDataCollection FROM TblProject'
          db = AccessDB(filename)
@@ -79,15 +88,25 @@ class TransferManager:
              
          project = self.addProject(projectname, startdate, enddate, description, currency)
              
-         query = '''INSERT INTO transferlog(pid,pid_access,projectname,datecollected,currency) 
-                      VALUES(%s,%s,'%s','%s','%s')''' % (project.pid, row.ProjectID, projectname, startdate, currency)
-                          
-         database = Database()
-         database.open()
-         database.execUpdateQuery( query )
-         database.close()
+         self.logTransfer(project.pid, row.ProjectID, projectname, startdate, currency)
          
          if not self.existsCurrency(row.Currency):
              self.addCurrency(row.Currency, row.Currency, row.Currency)
+             
+         self.transferHouseholds(filename, row.ProjectID, project.pid,  startdate)
+         
+     def transferHouseholds(self, accessfilename, sourcepid, targetpid, startdate):
+         query = "SELECT HHID, HHRealName FROM TblHouseholds WHERE ProjectID=%s" % sourcepid
+         
+         db = AccessDB(accessfilename)
+         db.open()
+         rows = db.execSelectQuery( query )
+         
+         project = self.getProject( targetpid )
+         
+         for row in rows:
+             project.addHousehold(row.HHID, row.HHRealName, startdate)
+             
+         db.close()
          
     
