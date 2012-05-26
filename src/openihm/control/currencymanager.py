cba4fa473c0e53f88ebbe7c74f888a26de4a01fc
John Pinner <funthyme@gmail.com>
2012-05-26 12:59:07 +0100

    Corrected indentation white space, stopped shadowing builtin id

--- src/openihm/control/currencymanager.py
+++ src/openihm/control/currencymanager.py
@@ -21,49 +21,50 @@ from model.database import Database
 from model.currency import Currency
 
 class CurrencyManager:
-     '''
-         Manages currencies. Allows adding, editing, deleting and retrieval of currencies.
-     '''   
-     def existsCurrency(self, name):
-         currency = self.getCurrencyByName(name)
-         if currency.name == "":
-             return False
-         else:
-             return True
-             
-     def getCurrencyByID(self,  currencyid):
-         currency = Currency(currencyid)
-         return currency
-         
-     def getCurrencyByName(self,  name):
-         currency = Currency(currencyname=name)
-         return currency
-        
-     def addCurrency(self,  currencyname,  abbreviation,  symbol ):
-         currency = Currency( 0 , currencyname,  abbreviation,  symbol )
-         return currency
-        
-     def editCurrency(self,  currencyid,  currencyname,  abbreviation,  symbol ):
-         currency = Currency(currencyid)
-         currency.editData( currencyname,  abbreviation,  symbol )
-        
-     def delCurrency(self,  currencyid):
-         query = "DELETE FROM currencies WHERE id=%i " % ( currencyid )
-         database = Database()
-         database.open()
-         database.execUpdateQuery( query )
-         database.close()
-        
-     def getCurrencies(self):        
-         query = "SELECT id FROM currencies"
-         self.database.open()
-         rows = self.database.execSelectQuery( query )
-         self.database.close()
-         currencies = []
-        
-         for row in rows:
-             id = row[0]
-             currency = Currency(id)
-             currencies.append( currency )
+    '''Manages currencies.
+    
+    Allows adding, editing, deleting and retrieval of currencies.
+    '''   
+    def existsCurrency(self, name):
+        currency = self.getCurrencyByName(name)
+        if currency.name == "":
+            return False
+        else:
+            return True
             
-         return currencies   
+    def getCurrencyByID(self,  currencyid):
+        currency = Currency(currencyid)
+        return currency
+        
+    def getCurrencyByName(self,  name):
+        currency = Currency(currencyname=name)
+        return currency
+       
+    def addCurrency(self,  currencyname,  abbreviation,  symbol ):
+        currency = Currency( 0 , currencyname,  abbreviation,  symbol )
+        return currency
+       
+    def editCurrency(self,  currencyid,  currencyname,  abbreviation,  symbol ):
+        currency = Currency(currencyid)
+        currency.editData( currencyname,  abbreviation,  symbol )
+       
+    def delCurrency(self, currencyid):
+        query = "DELETE FROM currencies WHERE id=%i " % ( currencyid )
+        database = Database()
+        database.open()
+        database.execUpdateQuery( query )
+        database.close()
+       
+    def getCurrencies(self):        
+        query = "SELECT id FROM currencies"
+        self.database.open()
+        rows = self.database.execSelectQuery( query )
+        self.database.close()
+        currencies = []
+       
+        for row in rows:
+            curr_id = row[0]
+            currency = Currency(curr_id)
+            currencies.append( currency )
+           
+        return currencies   
