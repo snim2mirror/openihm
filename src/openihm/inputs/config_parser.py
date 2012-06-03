d193933dcdf69b210a18709bbed776cf72eb188d
Colin Newell <colin.newell@gmail.com>
2012-06-03 16:04:35 +0100

    Fixed whitespace issues pep8 was whining about
    --HG--
    branch : colin/db_config

--- src/openihm/inputs/config_parser.py
+++ src/openihm/inputs/config_parser.py
@@ -1,15 +1,16 @@
 import ConfigParser
 
+
 class OpenIHMConfig(ConfigParser.SafeConfigParser):
 
     def __init__(self):
-        ConfigParser.SafeConfigParser.__init__(self, { 
+        ConfigParser.SafeConfigParser.__init__(self, {
             'host': 'localhost',
             'database': 'openihmdb',
             'user': 'openihm',
             'password': 'ihm2010',
             'port': '3306',
-            'superuser': 'root', 
+            'superuser': 'root',
             'superuser_password': '',
         },
         allow_no_value=True)
@@ -25,4 +26,3 @@ class OpenIHMConfig(ConfigParser.SafeConfigParser):
             'superuser':            self.get('database', 'superuser'),
             'superuser_password':   self.get('database', 'superuser_password'),
         }
-
