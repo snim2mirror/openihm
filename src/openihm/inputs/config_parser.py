0676c3397889782b8cf10dfbbbd5580bee74cdbf
Colin Newell <colin.newell@gmail.com>
2012-06-03 16:22:06 +0100

    Made config parser work without config file
    --HG--
    branch : colin/db_config

--- src/openihm/inputs/config_parser.py
+++ src/openihm/inputs/config_parser.py
@@ -4,7 +4,7 @@ import ConfigParser
 class OpenIHMConfig(ConfigParser.SafeConfigParser):
 
     def __init__(self):
-        ConfigParser.SafeConfigParser.__init__(self, {
+        self.defaults = {
             'host': 'localhost',
             'database': 'openihmdb',
             'user': 'openihm',
@@ -12,17 +12,21 @@ class OpenIHMConfig(ConfigParser.SafeConfigParser):
             'port': '3306',
             'superuser': 'root',
             'superuser_password': '',
-        },
-        allow_no_value=True)
+        }
+        ConfigParser.SafeConfigParser.__init__(self, self.defaults,
+                                                allow_no_value=True)
 
     def database_config(self):
-        # FIXME: need to deal gracefully with config file not existing.
-        return {
-            'host':                 self.get('database', 'host'),
-            'database':             self.get('database', 'database'),
-            'user':                 self.get('database', 'user'),
-            'password':             self.get('database', 'password'),
-            'port':                 self.get('database', 'port'),
-            'superuser':            self.get('database', 'superuser'),
-            'superuser_password':   self.get('database', 'superuser_password'),
-        }
+        try:
+            return {
+                'host':               self.get('database', 'host'),
+                'database':           self.get('database', 'database'),
+                'user':               self.get('database', 'user'),
+                'password':           self.get('database', 'password'),
+                'port':               self.get('database', 'port'),
+                'superuser':          self.get('database', 'superuser'),
+                'superuser_password': self.get('database',
+                                                  'superuser_password'),
+            }
+        except ConfigParser.NoSectionError, e:
+            return self.defaults
