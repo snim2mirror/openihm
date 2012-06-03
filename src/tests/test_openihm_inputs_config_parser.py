85f7b9a2d027b135539164ff06de41d355c04bbc
Colin Newell <colin.newell@gmail.com>
2012-06-03 17:26:26 +0100

    Added an extra test
    --HG--
    branch : colin/db_config

--- src/tests/test_openihm_inputs_config_parser.py
+++ src/tests/test_openihm_inputs_config_parser.py
@@ -1,5 +1,6 @@
 import unittest
 from openihm.inputs.config_parser import OpenIHMConfig
+import io
 
 
 class TestOpenIHMConfig(unittest.TestCase):
@@ -16,6 +17,25 @@ class TestOpenIHMConfig(unittest.TestCase):
             'port': '3306',
             'superuser': 'root',
             'superuser_password': '',
+        }
+        self.assertEqual(expected, open_ihm_config.database_config())
+
+    def test_read_config(self):
+        open_ihm_config = OpenIHMConfig()
+        config_file = """
+[database]
+superuser = master
+superuser_password = password
+        """
+        open_ihm_config.readfp(io.BytesIO(config_file))
+        expected = {
+            'host': 'localhost',
+            'database': 'openihmdb',
+            'user': 'openihm',
+            'password': 'ihm2010',
+            'port': '3306',
+            'superuser': 'master',
+            'superuser_password': 'password',
         }
         self.assertEqual(expected, open_ihm_config.database_config())
 
