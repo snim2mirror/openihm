f36cf1e49eb235daf65e4cbbaabb9956c72ac049
Colin Newell <colin.newell@gmail.com>
2012-07-17 23:18:43 +0100

    Ensure the port number coming from the config is a number and not a string
    --HG--
    branch : colin/db_config

--- src/openihm/inputs/config_parser.py
+++ src/openihm/inputs/config_parser.py
@@ -38,6 +38,7 @@ class OpenIHMConfig(ConfigParser.SafeConfigParser):
         }
         ConfigParser.SafeConfigParser.__init__(self, self.defaults,
                                                 allow_no_value=True)
+        self.defaults['port'] = int(self.defaults['port'])
 
     def database_config(self):
         """
@@ -50,18 +51,20 @@ class OpenIHMConfig(ConfigParser.SafeConfigParser):
                 'database': 'openihmdb',
                 'user': 'openihm',
                 'password': 'ihm2010',
-                'port': '3306',
+                'port': 3306,
                 'superuser': 'root',
                 'superuser_password': '',
             }
         """
+        # FIXME: should we also be returning utf8 and warnings params 
+        # here?
         try:
             return {
                 'host':               self.get('database', 'host'),
                 'database':           self.get('database', 'database'),
                 'user':               self.get('database', 'user'),
                 'password':           self.get('database', 'password'),
-                'port':               self.get('database', 'port'),
+                'port':               self.getint('database', 'port'),
                 'superuser':          self.get('database', 'superuser'),
                 'superuser_password': self.get('database',
                                                   'superuser_password'),
--- src/tests/test_openihm_inputs_config_parser.py
+++ src/tests/test_openihm_inputs_config_parser.py
@@ -2,25 +2,45 @@ import unittest
 from openihm.inputs.config_parser import OpenIHMConfig
 import io
 
-
 class TestOpenIHMConfig(unittest.TestCase):
     def test___init__(self):
         open_ihm_config = OpenIHMConfig()
 
     def test_database_config(self):
         open_ihm_config = OpenIHMConfig()
+        
         expected = {
             'host': 'localhost',
             'database': 'openihmdb',
             'user': 'openihm',
             'password': 'ihm2010',
-            'port': '3306',
+            'port': 3306,
             'superuser': 'root',
             'superuser_password': '',
         }
         self.assertEqual(expected, open_ihm_config.database_config())
 
-    def test_read_config(self):
+    def test_read_config_file(self):
+        open_ihm_config = OpenIHMConfig()
+        config_file = """
+[database]
+superuser = master
+superuser_password = password
+port = 3307
+        """
+        open_ihm_config.readfp(io.BytesIO(config_file))
+        expected = {
+            'host': 'localhost',
+            'database': 'openihmdb',
+            'user': 'openihm',
+            'password': 'ihm2010',
+            'port': 3307,
+            'superuser': 'master',
+            'superuser_password': 'password',
+        }
+        self.assertEqual(expected, open_ihm_config.database_config())
+
+    def test_read_config_file_port_default(self):
         open_ihm_config = OpenIHMConfig()
         config_file = """
 [database]
@@ -33,13 +53,13 @@ superuser_password = password
             'database': 'openihmdb',
             'user': 'openihm',
             'password': 'ihm2010',
-            'port': '3306',
+            'port': 3306,
             'superuser': 'master',
             'superuser_password': 'password',
         }
         self.assertEqual(expected, open_ihm_config.database_config())
 
-    def test_database_config(self):
+    def test_database_config_dbinfo(self):
         open_ihm_config = OpenIHMConfig()
         expected = {
             'host': 'localhost',
