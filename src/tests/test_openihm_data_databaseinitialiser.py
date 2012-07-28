412cf59704ee2d1e62f6317f4859795314ba4861
Colin Newell <colin.newell@gmail.com>
2012-07-28 17:55:54 +0100

    Added a bit of documentation
    --HG--
    branch : colin/db_config

--- src/tests/test_openihm_data_databaseinitialiser.py
+++ src/tests/test_openihm_data_databaseinitialiser.py
@@ -67,6 +67,23 @@ class TestDbConfig(unittest.TestCase):
         self.assertEqual(expected, db_config.superuser_dbinfo())
 
 class TestDatabaseInitialiser(unittest.TestCase):
+    """
+    In order to use these tests you need to have a config file named 
+    test_openihm.cfg in the directory you are running the tests from
+    (i.e. the src directory).
+
+        [database]
+        superuser = root
+        superuser_password = s00pers3cur3
+        database = test_openihm
+        user = openihm
+        password = ihm2012
+
+    This should contain database credentials for a database that exists
+    in mysql for testing.  This database will be toyed around with a lot.
+    Obviously avoid pointing it at a database you care about...
+
+    """
 
     def setUp(self):
         config = OpenIHMConfig()
