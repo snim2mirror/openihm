ed73b01a4685a8a8e8d128c046b985d90636b199
Colin Newell <colin.newell@gmail.com>
2012-07-29 12:26:06 +0100

    Pep8 and doc fixes
    --HG--
    branch : colin/db_config

--- src/openihm/data/config.py
+++ src/openihm/data/config.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 This file is part of open-ihm.
 
--*- coding: utf-8 -*-
 
 open-ihm is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
@@ -19,23 +18,22 @@ You should have received a copy of the GNU General Public License
 along with open-ihm.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+
 class Config(object):
     """ Configures the mysql database connector which is then called as below:
-    
         includes.mysql.connector.Connect(**Config.dbinfo())
     """
-    
-    """
-    Set the OpenIHMConfig object with the config options.
-    """
+
     @classmethod
     def set_config(cls, config_object):
+        """
+        Set the OpenIHMConfig object with the config options.
+        """
         cls.config = config_object
-    
-    """
-    Return the database configuration.
-    """
+
     @classmethod
     def dbinfo(cls):
+        """
+        Return the database configuration.
+        """
         return cls.config.dbinfo()
-    
