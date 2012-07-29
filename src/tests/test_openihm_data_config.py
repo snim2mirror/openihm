8ab720b04ea1d02bd0c769e9de17becd46788efc
Colin Newell <colin.newell@gmail.com>
2012-07-29 12:16:38 +0100

    Pep8 stuff
    --HG--
    branch : colin/db_config

--- src/tests/test_openihm_data_config.py
+++ src/tests/test_openihm_data_config.py
@@ -1,16 +1,19 @@
 import unittest
 from openihm.data.config import Config
 
+
 class dummy_config:
 
     def dbinfo(self):
-        return { 'a' : 'b' }
+        return {'a': 'b'}
+
 
 class TestConfig(unittest.TestCase):
 
     def test_dbinfo(self):
         Config.set_config(dummy_config())
-        self.assertEqual(Config.dbinfo(), { 'a' : 'b' })
+        self.assertEqual(Config.dbinfo(), {'a': 'b'})
+
 
 if __name__ == '__main__':
     unittest.main()
