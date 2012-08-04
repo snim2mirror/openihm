ed417771004166279a66c893e07059182ba5f82c
Colin Newell <colin.newell@gmail.com>
2012-08-04 19:55:38 +0100

    Started to write the database tests
    --HG--
    branch : colin/db_config

--- src/tests/test_openihm_gui_interface_mixins.py
+++ src/tests/test_openihm_gui_interface_mixins.py
@@ -1,10 +1,12 @@
 import unittest
 from openihm.gui.interface.mixins import *
+from database_helper import DatabaseHelper
 
 
 class Fake(TableViewMixin):
 
     pass
+
 
 class FakeMDI(MDIDialogMixin):
 
@@ -21,7 +23,7 @@ class FakeMDI(MDIDialogMixin):
 class FakeView(object):
 
     def selectedIndexes(self):
-        return [ FakeRow() for x in range(10) ]
+        return [FakeRow() for x in range(10)]
 
     def currentIndex(self):
         return FakeRow()
@@ -36,12 +38,13 @@ class FakeRow(object):
 class TestTableViewMixin(unittest.TestCase):
     def test_countRowsSelected(self):
         table_view_mixin = Fake()
-        import pdb; pdb.set_trace()
-        self.assertEqual(1, table_view_mixin.countRowsSelected(True))
+        view = FakeView()
+        self.assertEqual(1, table_view_mixin.countRowsSelected(view))
 
     def test_getCurrentRow(self):
         table_view_mixin = Fake()
-        self.assertEqual('test', table_view_mixin.getCurrentRow(tblVw))
+        view = FakeView()
+        self.assertEqual('test', table_view_mixin.getCurrentRow(view))
 
     def test_getSelectedRows(self):
         table_view_mixin = Fake()
@@ -51,35 +54,101 @@ class TestTableViewMixin(unittest.TestCase):
 
 class TestMDIDialogMixin(unittest.TestCase):
     def test_mdiClose(self):
-        # m_di_dialog_mixin = MDIDialogMixin()
-        # self.assertEqual(expected, m_di_dialog_mixin.mdiClose())
-        assert False # TODO: implement your test here
+        m_di_dialog_mixin = FakeMDI()
+        # FIXME: get more coverage
+        m_di_dialog_mixin.mdiClose()
 
     def test_setMdi(self):
-        # m_di_dialog_mixin = MDIDialogMixin()
-        # self.assertEqual(expected, m_di_dialog_mixin.setMdi(mdi))
-        assert False # TODO: implement your test here
+        m_di_dialog_mixin = FakeMDI()
+        m_di_dialog_mixin.setMdi(mdi)
+        self.assertEqual(expected, m_di_dialog_mixin.mdi)
+
 
 class TestMySQLMixin(unittest.TestCase):
+    """
+    In order to use these tests you need to have a config file named
+    test_openihm.cfg in the directory you are running the tests from
+    (i.e. the src directory).
+
+        [database]
+        superuser = root
+        superuser_password = s00pers3cur3
+        database = test_openihm
+        user = openihm
+        password = ihm2012
+
+    This should contain database credentials for a database that exists
+    in mysql for testing.  This database will be toyed around with a lot.
+    Obviously avoid pointing it at a database you care about...
+
+    """
+
+    def setUp(self):
+        import pdb; pdb.set_trace()
+        self.helper = DatabaseHelper(self)
+        self.helper.start_tests()
+        
+
+    def tearDown(self):
+        # drop the database
+        self.helper.end_tests()
+
     def test_executeMultipleResultsQueries(self):
         # my_sql_mixin = MySQLMixin()
         # self.assertEqual(expected, my_sql_mixin.executeMultipleResultsQueries(queries))
         assert False # TODO: implement your test here
 
     def test_executeMultipleUpdateQueries(self):
-        # my_sql_mixin = MySQLMixin()
-        # self.assertEqual(expected, my_sql_mixin.executeMultipleUpdateQueries(queries))
+        my_sql_mixin = MySQLMixin()
+        queries = [
+        """
+            insert into projects
+                (projectname, startdate, enddate, description, currency)
+            values
+                ('test', 2012-06-04, 2013-07-03, 'a simple test', 'GBP')""",
+        """
+            insert into projects
+                (projectname, startdate, enddate, description, currency)
+            values
+                ('test2', 2012-06-04, 2013-07-03, 'simple test', 'AUS')""",
+        ]
+        my_sql_mixin.executeMultipleUpdateQueries(queries)
+        expected = [
+            (u'test', None, None, u'a simple test', u'GBP'),
+            (u'test2', None, None, u'a simple test', u'GBP')
+        ]
+        query = """
+        select projectname, startdate, enddate, description, currency
+        from projects
+        """
+        # FIXME: the None's look hinky.
+        self.assertEqual(expected, my_sql_mixin.executeResultsQuery(query))
         assert False # TODO: implement your test here
 
     def test_executeResultsQuery(self):
-        # my_sql_mixin = MySQLMixin()
-        # self.assertEqual(expected, my_sql_mixin.executeResultsQuery(query))
-        assert False # TODO: implement your test here
+        my_sql_mixin = MySQLMixin()
+        my_sql_mixin.executeUpdateQuery("""
+            insert into projects
+                (projectname, startdate, enddate, description, currency)
+            values
+                ('test', 2012-06-04, 2013-07-03, 'a simple test', 'GBP')""")
+        expected = [(2, u'test', None, None, u'a simple test', u'GBP')]
+        query = 'select * from projects'
+        # FIXME: the None's look hinky.
+        self.assertEqual(expected, my_sql_mixin.executeResultsQuery(query))
 
     def test_executeUpdateQuery(self):
-        # my_sql_mixin = MySQLMixin()
-        # self.assertEqual(expected, my_sql_mixin.executeUpdateQuery(query))
-        assert False # TODO: implement your test here
+        my_sql_mixin = MySQLMixin()
+        my_sql_mixin.executeUpdateQuery("""
+            insert into projects
+                (projectname, startdate, enddate, description, currency)
+            values
+                ('test', 2012-06-04, 2013-07-03, 'a simple test', 'GBP')""")
+        expected = [(2, u'test', None, None, u'a simple test', u'GBP')]
+        query = 'select * from projects'
+        # FIXME: the None's look hinky.
+        self.assertEqual(expected, my_sql_mixin.executeResultsQuery(query))
+
 
 class TestDataEntryMixin(unittest.TestCase):
     def test_getDbString(self):
