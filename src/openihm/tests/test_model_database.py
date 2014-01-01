be7c1d061a1787d08eac273c48ecb5df5ba65538
Colin Newell <colin.newell@gmail.com>
2014-01-01 01:05:53 +0000

    Created tests for the final data access class I missed
    --HG--
    branch : sqlalchemy

---
+++ src/openihm/tests/test_model_database.py
@@ -0,0 +1,44 @@
+import unittest
+
+class TestModelDatabase(unittest.TestCase):
+    def test___init__(self):
+        # database = Database(host, database, user, password, port)
+        assert False # TODO: implement your test here
+
+    def test_close(self):
+        # database = Database(host, database, user, password, port)
+        # self.assertEqual(expected, database.close())
+        assert False # TODO: implement your test here
+
+    def test_databaseExists(self):
+        # database = Database(host, database, user, password, port)
+        # self.assertEqual(expected, database.databaseExists())
+        assert False # TODO: implement your test here
+
+    def test_databaseServerRunning(self):
+        # database = Database(host, database, user, password, port)
+        # self.assertEqual(expected, database.databaseServerRunning())
+        assert False # TODO: implement your test here
+
+    def test_execDefinitionQuery(self):
+        # database = Database(host, database, user, password, port)
+        # self.assertEqual(expected, database.execDefinitionQuery(query))
+        assert False # TODO: implement your test here
+
+    def test_execSelectQuery(self):
+        # database = Database(host, database, user, password, port)
+        # self.assertEqual(expected, database.execSelectQuery(query))
+        assert False # TODO: implement your test here
+
+    def test_execUpdateQuery(self):
+        # database = Database(host, database, user, password, port)
+        # self.assertEqual(expected, database.execUpdateQuery(query))
+        assert False # TODO: implement your test here
+
+    def test_open(self):
+        # database = Database(host, database, user, password, port)
+        # self.assertEqual(expected, database.open())
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
